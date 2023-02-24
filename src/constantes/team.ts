a98f897c28ad5de6d55a818e53993b37ff6468f5
Fabio Barros <fabio.junior643@outlook.com>
2023-02-24 11:09:09 -0300

    team constants

---
+++ src/constantes/team.ts
@@ -0,0 +1,38 @@
+interface ITeamData {
+  name: string;
+  functionName: string;
+  imageSrc: string;
+  instagramLink: string;
+  linkedinLink: string;
+  facebookLink: string;
+}
+
+export const teamData: ITeamData[] = [
+  {
+    name: 'Vanessa',
+    functionName: 'Advogada criminal',
+    imageSrc:
+      'https://assets-blog.hostgator.com.br/wp-content/uploads/2021/05/como-criar-site-para-advogado.webp',
+    instagramLink: 'https://www.instagram.com/',
+    linkedinLink: 'https://br.linkedin.com/',
+    facebookLink: 'https://pt-br.facebook.com/',
+  },
+  {
+    name: 'Pedro',
+    functionName: 'Advogado criminal',
+    imageSrc:
+      'https://fmp.edu.br/wp-content/uploads/2021/02/qual-e-o-papel-do-advogado-corporativo-a-fmp-te-mostra.jpg',
+    instagramLink: 'https://www.instagram.com/',
+    linkedinLink: 'https://br.linkedin.com/',
+    facebookLink: 'https://pt-br.facebook.com/',
+  },
+  {
+    name: 'Paulo',
+    functionName: 'Advogado criminal',
+    imageSrc:
+      'https://almeidaleal.com/wp-content/uploads/2022/05/advogado-do-futuro.jpg',
+    instagramLink: 'https://www.instagram.com/',
+    linkedinLink: 'https://br.linkedin.com/',
+    facebookLink: 'https://pt-br.facebook.com/',
+  },
+];
