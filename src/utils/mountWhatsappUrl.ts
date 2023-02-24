8716f637d43924f317693176f9a0bc51994f53df
Fabio Barros <fabio.junior643@outlook.com>
2023-02-24 16:36:10 -0300

    change whatsapp url

--- src/utils/mountWhatsappUrl.ts
+++ src/utils/mountWhatsappUrl.ts
@@ -3,7 +3,7 @@ import { IContactForm } from '../pages/home/sub-components/contact/ContactForm';
 export const mountWhatsappUrl = (values: IContactForm) => {
   let whatsappUrl = `https://wa.me/5548992007613/?text=`;
   const message = encodeURIComponent(
-    `Nome: ${values.name} \n E-mail: ${values.email} \n Mensagem: ${values.message}`,
+    `Nome: ${values.name}\nE-mail: ${values.email}\nMensagem: ${values.message}`,
   );
 
   whatsappUrl += message;
